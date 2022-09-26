a934904eb0964931fbe7f0bc4e4e71a3dbe77043
CarlaCCP <carldepaula230@gmail.com>
2022-09-26 18:45:17 -0300

    conexão com banco de dados + front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from ctypes import resize
 from flask import Flask, render_template, request, redirect, session, flash
+from flask_sqlalchemy import SQLAlchemy
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -12,6 +12,30 @@ listaJogos = [jogo1, jogo2]
 
 app = Flask(__name__)
 app.secret_key = 'blablabla'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/jogoteca'
+db = SQLAlchemy(app)
+
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    nome = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(40), nullable=False)
+    console = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class Usuarios(db.Model):
+    nickname = db.Column(db.String(8), nullable=False, primary_key = True)
+    nome = db.Column(db.String(20), nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+
+    #tipo o toString do java
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
 
 @app.route('/')
 def index():
